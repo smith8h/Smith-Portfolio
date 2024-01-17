e569588831aa91ea6a71ec76a38d020e8da6d648
Dev. Smith <husseinshakir81@gmail.com>
2024-01-17 12:24:17 +0300

    feat: complete experience section

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,45 @@
+
+const Data = [
+    {
+        id: 3,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2017 - 2021",
+        title: "Bachelor's Degree",
+        desc: "Biotechnology - Biology Department at the University of Mustansiriyah",
+    },
+    {
+        id: 2,
+        category: "education",
+        icon: "icon-briefcase",
+        year: "Oct 2021 - Dec 2022",
+        title: "Medical Analyst",
+        desc: "Experienced Medical Analyst: 1yr. Skilled with a microscope, fast and accurate result reading. Efficient work with genuine results. Excellent writing, and high accuracy standards.",
+    },
+    {
+        id: 1,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: " Feb 2019 - Present",
+        title: "Native Android Developer",
+        desc: "Talented Android app developer with 9+ open-source libraries on GitHub (@smith8h) and 5+ apps for private companies. Focused on crafting beautiful UI/UX for personal and client projects.",
+    },
+    {
+        id: 4,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "Oct 2023 - Present",
+        title: "Flutter App Developer",
+        desc: "",
+    },
+    {
+        id: 5,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "Jan 2024 - Precent",
+        title: "Front-End Developer",
+        desc: "",
+    },
+];
+
+export default Data;
