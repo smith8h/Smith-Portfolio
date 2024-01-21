ff27a9178c806280bf70daefcc81a9f4700afcb4
Dev. Smith <husseinshakir81@gmail.com>
2024-01-21 11:28:07 +0300

    feat: complete comments section

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -11,34 +11,34 @@ const Pricing = () => {
             <div className="pricing__container grid">
                 <div className="pricing__item">
                     <img src={Price1} alt="Price 1" className="pricing__img" />
-                    <h3 className="pricing__plan">Basic</h3>
-                    <p className="pricing__title">A Simple option but powerful to manage your business</p>
-                    <p className="pricing__support">Email support</p>
+                    <h3 className="pricing__plan">Basic Projects</h3>
+                    <p className="pricing__title">A Simple option but powerful to bring your project to life</p>
+                    <p className="pricing__support">No-Maintenance support</p>
                     <h3 className="price">
-                        <em>$</em> 18 <span>/ Month</span>
+                        <em>$</em>200 - 400<span>Project</span>
                     </h3>
-                    <a href="#" className="btn">Get Started</a>
+                    <a href="mailto:husseinshakir81@gmail.com" className="btn">Get Started</a>
                 </div>
                 <div className="pricing__item best">
                     <span className="recommended">Recommended</span>
                     <img src={Price2} alt="Price 2" className="pricing__img" />
-                    <h3 className="pricing__plan">Premium</h3>
-                    <p className="pricing__title">Unlimited product including app integration and more features</p>
-                    <p className="pricing__support">Mon-Fri support</p>
+                    <h3 className="pricing__plan">Premium Works</h3>
+                    <p className="pricing__title">Unlimited product including app integration and more features (Helpfull for graduation projects)</p>
+                    <p className="pricing__support">Semi-Maintenance support</p>
                     <h3 className="price">
-                        <em>$</em> 18 <span>/ Month</span>
+                        <em>$</em>400 - 3,100<span>Project</span>
                     </h3>
-                    <a href="#" className="btn">Get Started</a>
+                    <a href="mailto:husseinshakir81@gmail.com" className="btn">Get Started</a>
                 </div>
                 <div className="pricing__item">
                     <img src={Price3} alt="Price 3" className="pricing__img" />
-                    <h3 className="pricing__plan">Ultimate</h3>
+                    <h3 className="pricing__plan">Ultimate Projects</h3>
                     <p className="pricing__title">A wise option for large companies and individuals</p>
-                    <p className="pricing__support">24/7 support</p>
+                    <p className="pricing__support">24/7 support for the first 3-7 months</p>
                     <h3 className="price">
-                        <em>$</em> 18 <span>/ Month</span>
+                        <em>$</em>3,000 - 11,000<span>Projects</span>
                     </h3>
-                    <a href="#" className="btn">Get Started</a>
+                    <a href="mailto:husseinshakir81@gmail.com" className="btn">Get Started</a>
                 </div>
             </div>
         </section>
